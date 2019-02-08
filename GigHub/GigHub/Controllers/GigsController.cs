0b56d8784310f6fef42c5db95a1bc3e44f69da65
fahime <fahime.mostoufi@gmail.com>
2019-02-07 22:32:43 -0500

    Building a form with bootstrap

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.ViewModels;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+       
+        public ActionResult Create()
+        {
+            GigFormViewModel viewModel = new GigFormViewModel();
+            viewModel.Genres = _context.Genres;
+
+            return View(viewModel);
+        }
+
+
+    }
+}
