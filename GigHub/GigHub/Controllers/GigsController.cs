793d9e4c16d6d8432d0426d30d4963afb40ddecc
fahime <fahime.mostoufi@gmail.com>
2019-02-09 18:49:32 -0500

    Optimization of create Action in GigController

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,6 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
@@ -8,11 +10,13 @@ namespace GigHub.Controllers
     {
         private readonly ApplicationDbContext _context;
 
+      
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
-       
+
+        [Authorize]
         public ActionResult Create()
         {
             GigFormViewModel viewModel = new GigFormViewModel();
@@ -21,6 +25,25 @@ namespace GigHub.Controllers
             return View(viewModel);
         }
 
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0},{1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+       
 
     }
 }
