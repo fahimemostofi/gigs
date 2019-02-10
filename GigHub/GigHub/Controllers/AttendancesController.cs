267cbcdecef5a711bc82dc4cdc6f0b1bc714f22a
fahime <fahime.mostoufi@gmail.com>
2019-02-09 22:12:46 -0500

    adding Name to user registeration form

---
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+   
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+                _context=new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigId)
+        {
+            var attendance = new Attendance()
+            {
+                AttendeeId = User.Identity.GetUserId(),
+               
+                GigId = gigId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
